b9259ad74dafcdc03e20726d30f6573f4583eabf
bedlessdeus <bedlesssgod@gmail.com>
2025-05-05 10:00:57 +0200

    Updated config for comp with WebSpace

--- server/svelte.config.js
+++ server/svelte.config.js
@@ -1,9 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: "/proxy/5173",
+		}
+	}
 };
 
 export default config;
--- server/vite.config.ts
+++ server/vite.config.ts
@@ -1,7 +1,12 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: ['codeserver.shardp001.bedless.dev'],
+		host: '0.0.0.0', // make the dev server accessible externally
+		port: 5173 // or your custom port
+	}
 });
